54aeac7fac556c3bbf95faf6ab4d7cf31156a03f
Deeksha Kishore <diksha.kadri@gmail.com>
2020-04-01 17:21:02 +0530

    Added Child component in Admin for lazy loading

---
+++ src/app/core/layout/admin/admin2a.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './admin2a.component.html',
+  styleUrls: ['./admin2a.component.scss']
+})
+export class Admin2aComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/core/layout/admin/admin2b.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './admin2b.component.html',
+  styleUrls: ['./admin2b.component.scss']
+})
+export class Admin2bComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
