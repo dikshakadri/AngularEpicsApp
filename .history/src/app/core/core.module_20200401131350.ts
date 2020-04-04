9afc8dec90f13ba307fbd1d939b2a7344e0a507f
Deeksha Kishore <diksha.kadri@gmail.com>
2020-04-04 22:17:34 +0530

    removing history folder

--- .history/src/app/core/core.module_20200401131350.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule, BrowserModule]
-})
-export class CoreModule {}
